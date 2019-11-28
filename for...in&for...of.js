8bcf8605679cbba883f2c68a104386d365b061a6
= <=>
2019-11-28 12:39:46 +0400

    for...in&for...of

---
+++ for...in&for...of.js
@@ -0,0 +1,19 @@
+//let dizi = [3, 5, 7];
+//dizi.selam = "merhaba";   // [3, 5, 7,selam:"merhaba"]
+
+// for (let i in dizi) {
+//    console.log(i); // Çıktısı: "0", "1", "2", "selam"
+//    console.log(i,dizi[i]); // Çıktısı: "0 3", "1 5", "2 7", "selam merhaba"
+//    console.log(typeof i); //string tipi
+// }
+
+
+
+// for (let i of dizi) {
+//    console.log(i); // Çıktısı: 3, 5, 7
+//    console.log(typeof i); //degiskenin sahip old. tip
+// }
+
+// // Aşağıdaki örnekte, for...of döngüsü ve for...in döngüsü arasındaki fark gösterilmektedir. 
+// // for...in döngüsü nesne değişkenlerinin isimleri üzerinde gezerken, 
+// // for...of döngüsü ise değişkenlerin değerleri üzerinde gezer:
